f338a6cbed2e9f135ada86268bacc617a7cd03f6
ayan <mohdayanrehman07@gmail.com>
2024-05-09 11:09:58 +0530

    makeloginapi-with-install-json-web-token

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const userSignUpController = require("../controllers/userSignUp");
+
 const router = express.Router();
 
+const userSignUpController = require("../controllers/userSignUp");
+
+const userSignInController = require("../controllers/userSignIn");
+
 router.post("/signup", userSignUpController);
+router.post("/login", userSignInController);
 
 module.exports = router;
